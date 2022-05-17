6e64cac9b707e705f197728029067ae71256f346
Jakub <jakub.raban@gmail.com>
2022-05-17 20:54:44 +0200

    Fix disconnect crash error

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,9 +2,11 @@ import { deletePlayer, findAllPlayersInGame } from '../repositories/player'
 import { findGameById } from '../repositories/game'
 
 export const disconnect = async (socketId: string, gameId: string) => {
-  const game = (await findGameById(gameId)) as any
-  if (game.status === 'lobby' && socketId) {
-    await deletePlayer(socketId)
+  if (socketId && gameId) {
+    const game = (await findGameById(gameId)) as any
+    if (game.status === 'lobby' && socketId) {
+      await deletePlayer(socketId)
+    }
   }
   return findAllPlayersInGame(gameId)
 }
